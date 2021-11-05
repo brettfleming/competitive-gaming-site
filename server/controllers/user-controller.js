37478f1c389976696eafa6c95cad298da1ef853f
Brett Fleming <brettfleming015@gmail.com>
2021-11-05 12:33:31 -0500

    added the get single user controller

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -2,3 +2,16 @@ const { User } =require('../models');
 
 const { singToken } =require('../utils/auth');
 
+module.exports = {
+    async getSingleUser({ user = null, params }, res) {
+        const foundUser = await User.findOne({
+          $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
+        });
+    
+        if (!foundUser) {
+          return res.status(400).json({ message: 'Cannot find user' });
+        }
+    
+        res.json(foundUser);
+      }
+}
