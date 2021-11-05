91b7ec1a3357dab1bfa4fbc8ca0f49b36548c5bf
Brett Fleming <brettfleming015@gmail.com>
2021-11-05 12:34:22 -0500

    added the create user controller

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -13,5 +13,16 @@ module.exports = {
         }
     
         res.json(foundUser);
-      }
+      },
+      // create user, sign token, and send back (client/src/components/SignUpForm.js)
+  async createUser({ body }, res) {
+    // console.log(body);
+    const user = await User.create(body);
+    // console.log(user)
+    if (!user) {
+      return res.status(400).json({ message: 'User not created' });
+    }
+    const token = signToken(user);
+    res.json({ token, user });
+  },
 }
