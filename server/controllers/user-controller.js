f4c28e3f06e379a98960a9e13d0729ca619d5b9d
Brett Fleming <brettfleming015@gmail.com>
2021-11-05 12:36:59 -0500

    added the login controller

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -25,4 +25,19 @@ module.exports = {
     const token = signToken(user);
     res.json({ token, user });
   },
+  async login({ body }, res) {
+    const user = await User.findOne({ $or: [{ username: body.username }, { email: body.email }] });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const correctPw = await user.isCorrectPassword(body.password);
+
+    if (!correctPw) {
+      return res.status(400).json({ message: 'Invalid credentials' });
+    }
+    const token = signToken(user);
+    res.json({ token, user });
+  },
+
 }
